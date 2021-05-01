23b1ed3c7c2b78d6af7fae6c27b44fae193ab93e
shalinisuryavanshi17 <65484483+shalinisuryavanshi17@users.noreply.github.com>
2021-05-01 11:22:17 +0530

    commiting change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Emojis from "./components/EmojiDictionary";
 import emojis from "./emojis";
 function displayEmojis(emojis)
 {
  return(
-<dl className="dict">
-<h1>{emojis.title}</h1>
-<dd><h1>{emojis.emoji}</h1><p>{emojis.meaning}</p></dd>
-</dl>
+   <div className="box"><dl className="dict">
+   <dt><h1 id="title">{emojis.title}</h1></dt>
+   <dd><h1 className="emoji">{emojis.emoji}</h1><p className="meaning">{emojis.meaning}</p></dd>
+   </dl></div>
+
  )
 }
 export default function App() {
