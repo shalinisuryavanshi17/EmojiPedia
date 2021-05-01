1b9c07f1cedaa35fa4327fdab563b3966e84fd3f
shalinisuryavanshi17 <65484483+shalinisuryavanshi17@users.noreply.github.com>
2021-05-01 12:34:50 +0530

    styles added for boxes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+import Emojis from "./components/EmojiDictionary";
 import emojis from "./emojis";
 function displayEmojis(emojis)
 {
  return(
-   <div className="box"><dl className="dict">
-   <dt><h1 id="title">{emojis.title}</h1></dt>
-   <dd><h1 className="emoji">{emojis.emoji}</h1><p className="meaning">{emojis.meaning}</p></dd>
-   </dl></div>
-
+   <Emojis key={emojis.id} id={emojis.id} title={emojis.title} emoji={emojis.emoji} meaning={emojis.meaning}/>
  )
 }
 export default function App() {
 
   return (
     <div className="App">
-      <h1>Welcome To , My EmojiPedia 😊</h1>
-      {emojis.map(displayEmojis)}
+       <div><h1 className="heading">Welcome To , My EmojiPedia 😊</h1></div>
+      <div className="container" >{emojis.map(displayEmojis)}</div>
+     
+      
        </div>
   );
 }
--- src/components/EmojiDictionary.js
+++ src/components/EmojiDictionary.js
@@ -2,5 +2,13 @@ import React from "react"
 
 export default function Emojis(props)
 {
-return <h1>{props.title}</h1>
+return(
+  <div className="box"><dl className="dict">
+     
+     <div className="items"><dt><h1 id="title"><span>{props.id}</span>. {props.title}</h1></dt>
+   <dd><h1 className="emoji">{props.emoji}</h1><h4 className="meaning">{props.meaning}</h4></dd>
+   </div>
+   </dl></div>
+
+)
 }
